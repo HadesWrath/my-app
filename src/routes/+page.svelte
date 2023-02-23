76f7e16dcee06394be51ff0d7c3c32fdbc94f4d9
Hayden Smith (1000119848) <SMITHT7@student.op.ac.nz>
2023-02-24 11:51:04 +1300

    Created Epilepsy machine

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<!-- CSS Styling -->
+<style>
+
+    body
+    {
+        margin:0;
+        height: 100vh;
+        width: 100vw;
+    }
+    
+</style>
+
+<!-- JS Functionality -->
+<script>
+
+    let r;
+    let g;
+    let b;
+
+    let changeColor = () => {
+        r = Math.floor(Math.random()*255)
+        g = Math.floor(Math.random()*255);
+        b = Math.floor(Math.random()*255);
+    }
+
+    
+    setInterval(changeColor, 1/60)
+    
+</script>
+
+<!-- HTML - Structure -->
+<body style="background-color: rgb({r},{g},{b});">
+
+</body>
+
