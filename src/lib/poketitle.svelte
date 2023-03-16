51e65df185a9a30e3704581b042e5a70b446ebde
Hayden Smith (1000119848) <SMITHT7@student.op.ac.nz>
2023-03-17 11:58:46 +1300

    Feat: Add styling to scrollingMap

--- src/lib/poketitle.svelte
+++ src/lib/poketitle.svelte
@@ -1,3 +1,19 @@
 <h1>
     Who's That Pokemon? 
 </h1>
+
+<style>
+    @font-face {
+        font-family: Pokemon Hollow;
+        src: url('lib/fonts/pokemon_hollow-webfont.woff') format('woff');
+        src: url('lib/fonts/pokemon_hollow-webfont.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    h1
+    {
+        font-family: "Pokemon Hollow";
+        margin: 0 auto;
+    }
+</style>
--- src/lib/scrollingMap.svelte
+++ src/lib/scrollingMap.svelte
@@ -1,7 +1,32 @@
 <!-- Javascript -->
+<script>
+    import { tweened } from 'svelte/motion';
+    let styles = {
 
+    }
+    let mapX = tweened(0);
+    let mapY = tweened(-200);
+</script>
 
 <!-- HTML -->
 <main>
     <img src="fire red map.png" alt=""/>   
 </main>
+
+<!-- CSS -->
+<style>
+    main{
+        height: 100vh;
+        width: 100vw;
+        padding: 0;
+        border: 0;
+        
+    }
+
+    img{
+        overflow: hidden;
+        object-fit: none;
+        width: 100%;
+        object-position: 0px 0px;
+    }
+</style>
